9d4a0fcd4c04fd78d91b1d0db1b8e4bae6329ed6
lhalcomb <lhalcomb@drury.edu>
2024-10-30 18:37:21 -0500

    stuff looking better

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,6 @@ import connection from './database.js';
 const app = express();
 const port = 3000;
 
-app.get('/', (_req, res) => {
-    res.status(200).json({ message: 'Hello World' })
-});
-
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`)
 });
