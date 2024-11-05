55103a9d32763f3bee12ec2a71bf3dedb298cf9d
lhalcomb <lhalcomb@drury.edu>
2024-11-05 15:09:09 -0600

    did it

--- src/server.ts
+++ src/server.ts
@@ -5,9 +5,13 @@ import { router } from '../api/users.ts';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(router);
+
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`)
 });
+
 
 app.get('/users', async (_req, res) => {
     const [results, _fields] = await connection.query('SELECT * FROM User');
