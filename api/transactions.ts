1ddf78a44b3f60427305d56876e7d8b2e21e0ebb
lhalcomb <lhalcomb@drury.edu>
2024-11-20 19:19:28 -0600

    Got the token stuff done.

--- api/transactions.ts
+++ api/transactions.ts
@@ -10,35 +10,50 @@ async function getTransaction(user_id) {
 }
 
 async function createTransaction(user_id, req: Request, res: Response) {
-
     const { memo, title, amount, isPayment, isRecurring} = req.body;
     try {
         await connection.query("INSERT INTO Transactions (fk_User_id, memo, title, amount, isPayment, isRecurring) VALUES (?, ?, ?, ?, ?, ?)", [user_id, memo, title, amount, isPayment, isRecurring]);
         res.status(200).send({message: "Transaction posted successfully. "});
+        return;
     } catch {
+        console.log('five hundy')
         res.status(500).send({message: "Error creating transactions data. "});
+        return;
     }
 }
 
 router.post('/transactions', async (req: Request, res: Response) => {
     const user_id = await AuthorizeUser(res, req);
 
+    if (!user_id) {
+        return;
+    }
+
     console.log(user_id);
     if (!user_id){
         res.status(400).send({message: "User ID is required"});
+        return;
     }
     
     try {
         const transactions = await getTransaction(user_id);
         res.status(200).send(transactions);
+        return;
 
     } catch {
         res.status(500).send({message: "Error fetching transactions data. "});
+        return;
     }
 });
 
 router.post('/transaction', async (req: Request, res: Response) => {
-    const userId = AuthorizeUser(res, req);
+    const userId =  await AuthorizeUser(res, req);
+
+    if (!userId) {
+        console.log('no user id')
+        return;
+    }
+
     await createTransaction(userId, req, res);
 });
 
--- api/users.ts
+++ api/users.ts
@@ -19,18 +19,17 @@ async function decodeToken(token: any, secretKey){
 async function validCredentials(email, password, res: Response, secretKey) {
     const [users] = await connection.query("SELECT * from User where email = ?", [email]);
     
-    
     if (Array.isArray(users) && users.length === 0) {
         res.status(400).json({msg: "Invalid email or password"});
-        
         return;
     }
 
     const result = await bcrypt.compare(password, users[0].password);
-    
 
     if (result) {
-        const encodetoken = await encodeToken(users[0].User_id, secretKey);
+        const User_id = users[0].User_id;
+        await connection.query("UPDATE User SET last_login = ? WHERE User_id = ?", [new Date(), User_id]);
+        const encodetoken = await encodeToken(User_id, secretKey);
         res.status(200).json({msg: "Password is correct", token: encodetoken});
         return;
     }
@@ -44,14 +43,34 @@ async function validCredentials(email, password, res: Response, secretKey) {
 export async function AuthorizeUser(res: Response, req: Request){
     const token = req.body.token;
     const secretKey = process.env.SECRET_KEY;
+    const tokenTime = process.env.TOKEN_TIME;
     try{
         const decoded = await decodeToken(token, secretKey);
         if(decoded){
-            return decoded.UserId; 
+            const [users] = await connection.query("SELECT * FROM User where User_id = ? ", [decoded.UserId]);
+
+            if (Array.isArray(users) && users.length === 0) {
+                console.log('inval email or password')
+                res.status(400).json({msg: "Invalid email or password"});
+                return;
+            }
+
+            const last_login = users[0].last_login;
+            console.log(Date.now() - new Date(last_login).getTime());
+
+            if (Date.now() - new Date(last_login).getTime() > Number(tokenTime) * 1000) {
+                console.log('expired')
+                res.status(401).json({msg: "Token has expired. Please login again."});
+                return;
+            }
+
+            return decoded.UserId;
         }
     }
     catch(err){
+        console.log('err')
         res.status(401).json({msg: "Decode Token Error"});
+        return;
     }
    
 }
@@ -64,7 +83,23 @@ router.post('/auth', async (req: Request, res: Response) => {
         await validCredentials(email, password, res, secretKey);
     } catch(err) {
         res.status(500).json(err);
-        console.log(email);
+        
+        return;
+    }
+});
+
+router.post('/checkToken', async (req: Request, res: Response) => {
+    console.log('CHECK TOKEN')
+    try {
+        const user_id = await AuthorizeUser(res, req);
+
+        if (!user_id) {
+            return;
+        }
+        res.status(200).json({ msg: "Yipeee" });
+        return;
+    } catch(err) {
+        res.status(500).json(err);
         return;
     }
 });
